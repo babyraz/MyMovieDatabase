d38051095c539939e38ebc8e795618ea883925d3
babyraz <156018892+babyraz@users.noreply.github.com>
2025-02-27 15:11:27 +0100

    Update favorites.js

--- template/scripts/modules/favorites.js
+++ template/scripts/modules/favorites.js
@@ -1,28 +1,4 @@
-
-
 const favoritesList = JSON.parse(localStorage.getItem('favorites')) || [];
-
-/*export function addToFavorites(movieID, button) {
-    let favoritesList = JSON.parse(localStorage.getItem('favorites')) || [];
-    if (!favoritesList.includes(movieID)) {
-        // Add movie to favorites
-        favoritesList.push(movieID);
-        console.log(`${movieID} added to favorites`);
-        button.textContent = "Remove from favorites";  // Change button text
-    } else {
-        // Remove movie from favorites
-        const index = favoritesList.indexOf(movieID);
-        if (index > -1) {
-            favoritesList.splice(index, 1);
-            console.log(`${movieID} removed from favorites`);
-        }
-        button.textContent = "Add to favorites";  // Change button text back
-    }
-
-    // Update localStorage with the new list
-    localStorage.setItem('favorites', JSON.stringify(favoritesList));
-    console.log(favoritesList);
-}*/
 
 export async function addToFavorites(movieID) {
     let favoritesList = JSON.parse(localStorage.getItem('favorites')) || [];  // Get current favorites
@@ -42,4 +18,3 @@ export function getFavorites() {
     const favorites = localStorage.getItem('favorites');
     return favorites ? JSON.parse(favorites) : [];
 }
-
